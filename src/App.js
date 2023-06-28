5ee76e05d6186c76834d31fe706cf6794c6e07d7
Ekam-Bitt <ekambitt@gmail.com>
2023-06-28 17:47:47 +0530

    created pages and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
 function App() {
   return (
     <>
       <h1 className=" underline">Hello world!</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/Profile' element={<Profile/>}></Route>
+          <Route path='/Sign-In' element={<SignIn/>}></Route>
+          <Route path='/Sign-Up' element={<SignUp/>}></Route>
+          <Route path='/Forgot-Password' element={<ForgotPassword/>}></Route>
+          <Route path='/Offers' element={<Offers/>}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>This line is being routed from Home</div>
+  )
+}
