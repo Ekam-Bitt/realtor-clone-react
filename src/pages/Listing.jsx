25a7976ad9910a53490f08afb0674cb26d86e430
Ekam-Bitt <ekambitt@gmail.com>
2024-04-28 19:14:42 +0530

    add map using leaflet and react-leaflet

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -5,6 +5,7 @@ import Spinner from "../components/Loading";
 import { db } from "../firebase";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation, Pagination, EffectFade } from "swiper/modules";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "swiper/css/bundle";
 import {
   FaShare,
@@ -159,20 +160,39 @@ export default function Listing() {
             </li>
           </ul>
           {listing.userRef !== auth.currentUser?.uid && !contactLandlord && (
-             <div className="mt-6">
-               <button
-                 onClick={() => setContactLandlord(true)}
-                 className="px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg w-full text-center transition duration-150 ease-in-out "
-               >
-                 Contact Landlord
-               </button>
-             </div>
-           )}
-           {contactLandlord && (
-             <Contact userRef={listing.userRef} listing={listing} />
-           )}
+            <div className="mt-6">
+              <button
+                onClick={() => setContactLandlord(true)}
+                className="px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg w-full text-center transition duration-150 ease-in-out "
+              >
+                Contact Landlord
+              </button>
+            </div>
+          )}
+          {contactLandlord && (
+            <Contact userRef={listing.userRef} listing={listing} />
+          )}
         </div>
-        <div className="bg-blue-300 w-full z-10 overflow-x-hidden">map</div>
+        <div className="bg-blue-300 w-full z-10 overflow-x-hidden">
+          <MapContainer
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: "100%", width: "100%" }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>
+                {listing.address}
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
       </div>
     </main>
   );
