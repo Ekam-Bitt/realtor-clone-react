aba7ee2718d760efa5b53a98ca3d33557dd79f6f
Ekam Bitt <ekambitt@gmail.com>
2024-06-19 04:58:19 +0530

    create rent and sale pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SignUp from "./pages/SignUp";
 import PrivateRoute from "./components/PrivateRoute";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
+import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
 import EditListing from "./pages/EditListing";
 import Header from "./components/Header";
@@ -18,26 +19,24 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/Profile" element={<PrivateRoute />}>
-            <Route path="/Profile" element={<Profile />}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
           </Route>
-          <Route path="/Sign-In" element={<SignIn />}></Route>
-          <Route path="/Sign-Up" element={<SignUp />}></Route>
-          <Route path="/Forgot-Password" element={<ForgotPassword />}></Route>
-          <Route path="/Offers" element={<Offers />}></Route>
-          <Route path="/Create-Listing" element={<PrivateRoute />}>
-            <Route path="/Create-Listing" element={<CreateListing />}></Route>
-          </Route>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route
             path="/category/:categoryName/:listingId"
             element={<Listing />}
           />
-          <Route path="/Edit-Listing" element={<PrivateRoute />}>
-            <Route
-              path="/Edit-Listing/:listingId"
-              element={<EditListing />}
-            ></Route>
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
+          <Route path="create-listing" element={<PrivateRoute />}>
+            <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
+          <Route path="edit-listing" element={<PrivateRoute />}>
+            <Route path="/edit-listing/:listingId" element={<EditListing />} />
           </Route>
         </Routes>
       </Router>
